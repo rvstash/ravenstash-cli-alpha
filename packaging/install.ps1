f9650426829b51ccb0389ebc868554dd5d1916f5
ixunio <38986504+ixunio@users.noreply.github.com>
2026-09-15 15:10:17 +0000

    release: prepare RVS 0.13.6rc1

--- packaging/install.ps1
+++ packaging/install.ps1
@@ -6,7 +6,7 @@ param(
 )
 
 $ErrorActionPreference = "Stop"
-$ReleaseVersion = "0.13.5"
+$ReleaseVersion = "0.13.6rc1"
 $CompatibilityChannel = "v0.13"
 $Repository = "rvstash/ravenstash-cli-alpha"
 $ReleaseBase = "https://github.com/$Repository/releases/download/v$ReleaseVersion"
